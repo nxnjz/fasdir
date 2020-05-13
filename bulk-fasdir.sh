d5d8c2958956ea6f77fd2d9ec3c0d77b84df09f5
nxnjz <nxnjz@nxnjz.net>
2020-05-13 03:32:35 +0300

    update

--- bulk-fasdir.sh
+++ bulk-fasdir.sh
@@ -4,6 +4,7 @@ while true
 do
   read -p "enter url scheme: " scheme
   read -p "enter host: " host
-  fasdir -w ~/wl/web-content/nxnjzhttp.txt --random-user-agent -t 20 -x ,/,.txt,.json,.html,.xml,.js,.php,.asp,.aspx,.htm,.jsp -o fasdirbulk.tmp -O -s 1-403,405-999 -u $scheme$host
+  fasdir -w ~/wl/web-content/nxnjzhttp.txt --random-user-agent -t 60 -x ,/,.txt,.json,.html,.xml,.js,.php,.asp,.aspx,.htm,.jsp -o fasdirbulk.tmp -O -s 1-403,405-999 -u $scheme$host
   mv fasdirbulk.tmp $host.fasdir.tmp
+  echo output at $host.fasdir.tmp
 done
