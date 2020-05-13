d42a2f702004be13f157c0943d305116e80af08e
nxnjz <nxnjz@nxnjz.net>
2020-05-13 04:38:39 +0300

    update

--- bulk-fasdir.sh
+++ bulk-fasdir.sh
@@ -6,7 +6,7 @@ do
   read -p "enter host: " host
   read -p "enter base path: " path
   fasdir -w ~/wl/web-content/nxnjzhttp.txt --random-user-agent -t 60 -x ,/,.txt,.json,.html,.xml,.js,.php,.asp,.aspx,.htm,.jsp -o $host.fasdir.tmp -s 1-403,405-999 -u $scheme$host$path
-  output = $host.fasdir$RANDOM.txt
+  output=$host.fasdir$RANDOM.txt
   mv $host.fasdir.tmp $output
   echo output at $output
 done
