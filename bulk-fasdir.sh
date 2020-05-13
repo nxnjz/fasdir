2713f829b363f5da1136684bc89a14a9dc600856
nxnjz <nxnjz@nxnjz.net>
2020-05-13 03:27:33 +0300

    added q&d script for bulk

---
+++ bulk-fasdir.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+while true
+do
+  read -p "enter url scheme: " scheme
+  read -p "enter host: " host
+  fasdir -w ~/wl/web-content/nxnjzhttp.txt --random-user-agent -t 20 -x ,/,.txt,.json,.html,.xml,.js,.php,.asp,.aspx,.htm,.jsp -o fasdirbulk.tmp -O -s 1-403,405-999 -u $scheme$host
+  mv fasdirbulk.tmp $host.fasdir.tmp
+done
